a0e8f989b9e9c3353317ee9a91d87b3705eb7462
Asta Martin <martin.asta@hotmail.com>
2023-02-13 19:18:22 +0100

    pre merge

--- AstaMartin&CopinThomas/demineur.js
+++ AstaMartin&CopinThomas/demineur.js
@@ -1,7 +1,7 @@
 /**Fonction principale qui s'occupe de set up la grille et du bon déroulement de la partie.
  * 
  */
-function main(){
+function main() {
     let sizeOfGrid = parseInt(prompt("Quelle sera la hauteur de votre grille ? (la grille sera carrée)"));
     createGrid(sizeOfGrid);
 }
@@ -11,9 +11,9 @@ function main(){
  * @param {*} numberOfBombs indique le nombre de bombes que l'on souhaite mettre dans la grille
  * @returns une liste de position où on ajoutera des bombes
  */
-function setBombs(numberOfBombs){
+function setBombs(numberOfBombs) {
     let posOfBombs = [];
-    for(let i = 0; i < numberOfBombs; i++){
+    for (let i = 0; i < numberOfBombs; i++) {
         posOfBombs.push([]);
         posOfBombs[i].push(Math.floor(Math.random() * (5 - 0)) + 0);
         posOfBombs[i].push(Math.floor(Math.random() * (5 - 0)) + 0);
