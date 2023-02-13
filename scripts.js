60556564c97bc195d8dd5dfa727550551602eb58
Beatwen <c_bouserez@hotmail.com>
2023-02-13 20:07:33 +0100

    Choix Difficulté

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
 let btns = document.querySelectorAll(".btn");
 let listBomb = [];
-document.getElementById("validBomb").addEventListener("click", defineBomb);
-function defineBomb() {
-  let nbBomb = parseInt(document.querySelector("#nbBomb").value);
-  if (nbBomb > 9 || nbBomb < 1) {
-    alert("NTM")
-  } else {
+
+document
+  .querySelectorAll("#level")
+  .forEach((x) => x.addEventListener("click", defineBomb));
+function defineBomb(e) {
+    let nbBomb = 0;
+  if (document.querySelector("#level")) {
+    switch (e.target.innerHTML) {
+      case "Easy":
+        nbBomb = 5;
+        break;
+      case "Middle":
+        nbBomb = 10;
+        break;
+      case "Hard":
+        nbBomb = 15;
+        break;
+      case "Extreme":
+        nbBomb = 20;
+        break;
+        
+    }
+  }
     while (listBomb.length < parseInt(nbBomb)) {
       let newBomb = Math.floor(Math.random() * nbBomb);
       console.log(Math.floor(Math.random() * nbBomb));
@@ -15,4 +32,3 @@ function defineBomb() {
       }
     }
   }
-}
