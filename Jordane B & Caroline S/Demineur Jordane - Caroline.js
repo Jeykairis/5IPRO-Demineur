0b4e9d8c196dc8239e7816881c52c13d73c5535c
rokaline <caroline.setton@ifosup.be>
2023-02-20 21:21:32 +0100

    MAJ 21h21

--- Jordane B & Caroline S/Demineur Jordane - Caroline.js
+++ Jordane B & Caroline S/Demineur Jordane - Caroline.js
@@ -2,47 +2,16 @@
 /* Code du démineur*/
 
 
-var i= 0; // boucle
-var min = 0 ; //permet de calculer %
-var max = 10 ; //permet de calculer %
-var game;
-var random; // permet de mettre la fonction aléatoire
+// A) Sélectionner l'élément input et récupérer sa valeur dans le premier bouton
 
-
-
-
-console.log("hello world");
-
-
-var game = prompt("Voulez-vous jouer au Démineur?"); 									// le user souhaite jouer? OUI/NON
-
-	if(game === "oui"){ 																	//Si oui
-		do {																				// bloc qui exécute les tâches tant que le joueur indique qu'il veut jouer
-			var choice = prompt("Choisissez le nombre de bombes souhaitées: entre 1 et 10.");  // le user doit choisir le nbre de bombes (max 10)
-			if( choice > 0 || choice < 11 ){
-			
-                // ici Créer une fonction qui va mettre aléatoirement le nb de nombre dans le tableau 
-
-				}
-				else{
-					
-					console.log(choice + " Veuillez encoder un nombre comme demandé svp.") 				// dans le cas de nombre incorrect
-				
-				}
-
-			var game = prompt("Voulez-vous rejouer?"); 										//permet de rejouer 
-			
-			}
-			
-
-		}
-		while(game == "oui"); 																// les tâches sont exécutées tant que le user veut jouer
-		
-		console.log("Merci d'avoir joué. Votre taux de réussite est de: " + (resultWin/i)*100 + "%."); 
+function getValue(){
 	
-
-	else{
-		alert("Merci pour votre réponse. A bientôt"); // le user ne souhaite pas jouer dès la première question
-	}
-
+		var input = document.getElementById("in").value;
+		document.getElementById("button2").innerHTML = input;
+	
+}
+                    
+                    
+                    
+//B) injecter la valeur dans le tableau de boutons
 	
