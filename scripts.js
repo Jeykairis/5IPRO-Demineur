365fb7d4aaead9373fe3a3751129d1beb780b7c4
Beatwen <c_bouserez@hotmail.com>
2023-02-26 19:37:38 +0100

    V1 MineSweeperWorking

--- scripts.js
+++ scripts.js
@@ -1,32 +1,58 @@
 let mainTab = document.getElementById("mainTab");
 let tab = [];
+let listBomb = [];
+let difficulty = ""
+let size = 0
+let nbBombs = 0
 
+document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+});
+document.getElementById("smileyButton").addEventListener("click", () => { tabF(size);
+                                                                        setTimeout(() => defineBomb(nbBombs, size), 50);
+                                                                        })
 document.querySelectorAll(".level").forEach((button) =>
   button.addEventListener("click", (e) => {
     defineDifficulty(e);
   })
 );
 function defineDifficulty(e) {
+  console.log("defineDifficulty");
   switch (e.target.innerHTML) {
     case "Easy":
-      tabF(4);
-      setTimeout(() => defineBomb(5, 4), 50);
+      difficulty = "Easy"
+      size = 9
+      nbBombs = 10
+      tabF(size);
+      setTimeout(() => defineBomb(nbBombs, size), 50);
       break;
     case "Medium":
-      tabF(15);
-      setTimeout(() => defineBomb(10, 15), 50);
+      difficulty = "Medium"
+      size = 16
+      nbBombs = 40
+      tabF(size);
+      setTimeout(() => defineBomb(nbBombs, size), 50);
       break;
     case "Hard":
-      tabF(30);
-      setTimeout(() => defineBomb(100, 30), 50);
+      difficulty = "Hard"
+      size = 21
+      nbBombs = 99
+      tabF(size);
+      setTimeout(() => defineBomb(nbBombs, size), 50);
       break;
     case "Extreme":
-      tabF(60);
-      setTimeout(() => defineBomb(1000, 60), 50);
+      difficulty = "Extreme"
+      size = 27
+      nbBombs = 150
+      tabF(size);
+      setTimeout(() => defineBomb(nbBombs, size), 50);
       break;
   }
 }
-const tabF = (size) => {
+const tabF = (size) => { 
+  document.getElementById("smileyImage").src="images/smileyHappy.png";
+  document.getElementById("winText").innerHTML = ""
+  document.getElementById("smileyButton").removeAttribute("hidden")
   tab = [];
   for (let i = 0; i < size; i++) {
     let row = [];
@@ -35,14 +61,13 @@ const tabF = (size) => {
     }
     tab.push(row);
   }
-  console.log(tab);
   return tab;
 };
 
 function defineBomb(nbBombs, size) {
-  console.log(tab);
+  listBomb = [];
+  
   let i = 0;
-  let listBomb = [];
   while (i < nbBombs) {
     let rowBomb = Math.floor(Math.random() * size);
     let colBomb = Math.floor(Math.random() * size);
@@ -52,7 +77,8 @@ function defineBomb(nbBombs, size) {
       i++;
     }
   }
-  checkNbBomb(tab);
+  console.log(listBomb);
+  createTab();
 }
 function createTab() {
   mainTab.innerHTML = "";
@@ -63,7 +89,7 @@ function createTab() {
       const cell = document.createElement("td");
       const buttons = document.createElement("button");
       buttons.classList = "btn";
-      buttons.id = `[${i}][${j}]`;
+      buttons.id = `${i}-${j}`;
       buttons.dataset.rowIndex = i;
       buttons.dataset.colIndex = j;
       cell.appendChild(buttons);
@@ -76,50 +102,6 @@ function createTab() {
   });
   console.table(tab);
 }
-
-function checkMines(row, col) {
-  let mine = 0
-
-  
-
-}
-
-
-/*function checkNbBomb() {
-  for (let i = 0; i < tab.length; i++) {
-    for (let j = 0; j < tab.length; j++) {
-      if (tab[i][j] === "b") {
-        if (i > 0 && typeof tab[i - 1][j] === "number") tab[i - 1][j]++;
-        if (i > 0 && j > 0 && typeof tab[i - 1][j - 1] === "number")
-          tab[i - 1][j - 1]++;
-        if (
-          i > 0 &&
-          j < tab[i].length - 1 &&
-          typeof tab[i - 1][j + 1] === "number"
-        )
-          tab[i - 1][j + 1]++;
-        if (j > 0 && typeof tab[i][j - 1] === "number") tab[i][j - 1]++;
-        if (j < tab[i].length - 1 && typeof tab[i][j + 1] === "number")
-          tab[i][j + 1]++;
-        if (
-          i < tab.length - 1 &&
-          j > 0 &&
-          typeof tab[i + 1][j - 1] === "number"
-        )
-          tab[i + 1][j - 1]++;
-        if (i < tab.length - 1 && typeof tab[i + 1][j] === "number")
-          tab[i + 1][j]++;
-        if (
-          i < tab.length - 1 &&
-          j < tab[i].length - 1 &&
-          typeof tab[i + 1][j + 1] === "number"
-        )
-          tab[i + 1][j + 1]++;
-      }
-    }
-  }
-  createTab(tab);
-}
 function clic(e) {
   const { rowIndex, colIndex } = e.target.dataset;
   const row = parseInt(rowIndex);
@@ -127,15 +109,95 @@ function clic(e) {
   const num = tab[row][col];
   if (e.button == 0) {
     if (num === "b") {
+      gameOver()
       console.log("boom!");
       e.target.style.backgroundImage = "url('images/boom.png')";
+
     } else {
-      e.target.style.background = "transparent";
-      if (num > 0) {
-        e.target.innerHTML = num;
-      } else {
+      checkMines(row, col, e);
+    }
+  } else if (e.button == 2) {
+    if (e.target.innerText == "") {
+      e.target.innerText = "🚩"
+      document.getElementById(row + "-" + col).classList.add("clicked");
+      
+    }
+    else if (e.target.innerText == "🚩") {
+      e.target.innerText = "?";
+      document.getElementById(row + "-" + col).classList.remove("clicked");
+    }
+    else if (e.target.innerText == "?") {
+      e.target.innerText = "";
+    }
+  }  
+  let nbClicked = document.querySelectorAll(".clicked")
+  let nbFlag = 0
+  document.querySelectorAll(".clicked").forEach( (x) => x.innerHTML.includes("🚩") ? nbFlag++: null);
+  console.log(nbFlag);
+  if (nbClicked.length == (size*size)  && nbFlag == 10) {
+    document.getElementById("smileyImage").src="images/smileyWin.png";
+    document.querySelectorAll(".btn").forEach((x) => {
+      x.removeEventListener("mousedown", clic);
+    });
+    document.getElementById("winText").innerHTML = "You win ! Nicely done"
+  }
+}
+function checkMines(row, col, e) {
+  if (row < 0 || col < 0 || row >= tab.length || col >= tab.length) {
+    return;
+  }
+
+  if (document.getElementById(row + "-" + col).classList.contains("clicked")) {
+    return;
+  }
+  document.getElementById(row + "-" + col).classList.add("clicked");
+  document.getElementById(row + "-" + col).style.background = "lightgrey"
+
+
+  let mine = 0;
+  mine += checkTile(row - 1, col - 1);
+  mine += checkTile(row - 1, col);
+  mine += checkTile(row - 1, col + 1);
+  mine += checkTile(row, col - 1);
+  mine += checkTile(row, col + 1);
+  mine += checkTile(row + 1, col - 1);
+  mine += checkTile(row + 1, col);
+  mine += checkTile(row + 1, col + 1);
+  if (mine > 0) {
+    document.getElementById(row + "-" + col).innerHTML = mine;
+  } else {
+    checkMines(row - 1, col - 1, e);
+    checkMines(row - 1, col, e);
+    checkMines(row - 1, col + 1, e);
+    checkMines(row, col - 1, e);
+    checkMines(row, col + 1, e);
+    checkMines(row + 1, col - 1, e);
+    checkMines(row + 1, col, e);
+    checkMines(row + 1, col + 1, e);
+  }
+}
+function checkTile(row, col) {
+  if (row < 0 || col < 0 || row >= tab.length || col >= tab.length) {
+    return 0;
+  }
+  if (tab[row][col] == "b") {
+    return 1;
+  }
+  return 0;
+}
+function gameOver() {
+  seeAllBombs()
+  document.querySelectorAll(".btn").forEach((x) => {
+    x.removeEventListener("mousedown", clic);
+  });
+}
+function seeAllBombs() {
+    for (let i = 0; i < tab.length; i++) {
+      for (let j = 0; j < tab.length; j++) {
+       if (listBomb.includes(tab[i][j])) {
+        document.getElementById(i + "-" + j).style.backgroundImage = "url('images/boom.png')";
+       };
       }
     }
-  } else if (e.button == 1) {
-  }
-}*/
+    document.getElementById("smileyImage").src="images/smileyDead.png";
+  };
