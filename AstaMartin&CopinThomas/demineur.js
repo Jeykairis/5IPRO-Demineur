212766490b967ff43d3d2ee36ad8614c185e7d78
Asta Martin <martin.asta@hotmail.com>
2023-03-04 15:41:31 +0100

    Ajout de la fonction displayGrid qui affiche le contenu de la grille + ajout d'un favicon

--- AstaMartin&CopinThomas/demineur.js
+++ AstaMartin&CopinThomas/demineur.js
@@ -6,9 +6,10 @@ function main() {
     let grid = createGrid(sizeOfGrid);
     let bombs = setBombs(sizeOfGrid);
     for (let i = 0; i < bombs.length; i++) {
-        grid[bombs[i][0]][bombs[i][1]] = "X";
+        grid[bombs[i][0]][bombs[i][1]] = 9;
     }
     fixeIndexes(grid, bombs);
+    displayGrid(grid);
     console.log(grid);
 }
 
@@ -20,9 +21,9 @@ function main() {
 function setBombs(numberOfBombs) {
     let posOfBombs = [];
     let i = 0;
-    while(posOfBombs.length < numberOfBombs){
+    while (posOfBombs.length < numberOfBombs) {
         let newPos = [Math.floor(Math.random() * (numberOfBombs - 0)) + 0, Math.floor(Math.random() * (numberOfBombs - 0)) + 0];
-        if(!includesPair(posOfBombs,newPos)){
+        if (!includesPair(posOfBombs, newPos)) {
             posOfBombs.push(newPos);
             i++;
         }
@@ -36,9 +37,9 @@ function setBombs(numberOfBombs) {
  * @param {*} pair indique la position à check dans la liste
  * @returns true si la paire est déjà dans la liste, false sinon
  */
-function includesPair(posOfBombs, pair){
-    for(let i = 0; i < posOfBombs.length; i++){
-        if(pair[0] == posOfBombs[i][0] && pair[1] == posOfBombs[i][1]){
+function includesPair(posOfBombs, pair) {
+    for (let i = 0; i < posOfBombs.length; i++) {
+        if (pair[0] == posOfBombs[i][0] && pair[1] == posOfBombs[i][1]) {
             return true;
         }
     }
@@ -67,31 +68,46 @@ function createGrid(a) {
  * @param {*} bombs indique la liste de positions des bombes
  */
 function fixeIndexes(grid, bombs) {
-    for (let i = 0; i < bombs.length; i++){ //On a la liste des positions des bombes, du coup on s'occupe d'incrémenter les indices autour de chaque bombe
-        if (bombs[i][1] > 0 && grid[bombs[i][0]][bombs[i][1]-1] != "X"){ //Les 8 positions autour du coup oui
-            grid[bombs[i][0]][bombs[i][1]-1] += 1;
+    for (let i = 0; i < bombs.length; i++) { //On a la liste des positions des bombes, du coup on s'occupe d'incrémenter les indices autour de chaque bombe
+        if (bombs[i][1] > 0 && grid[bombs[i][0]][bombs[i][1] - 1] != 9) { //Les 8 positions autour du coup oui
+            grid[bombs[i][0]][bombs[i][1] - 1] += 1;
         }
-        if (bombs[i][1] > 0 && bombs[i][0] < grid.length-1 && grid[bombs[i][0]+1][bombs[i][1]-1] != "X"){
-            grid[bombs[i][0]+1][bombs[i][1]-1] += 1;
+        if (bombs[i][1] > 0 && bombs[i][0] < grid.length - 1 && grid[bombs[i][0] + 1][bombs[i][1] - 1] != 9) {
+            grid[bombs[i][0] + 1][bombs[i][1] - 1] += 1;
         }
-        if (bombs[i][0] < grid.length-1 && grid[bombs[i][0]+1][bombs[i][1]] != "X"){
-            grid[bombs[i][0]+1][bombs[i][1]] += 1;
+        if (bombs[i][0] < grid.length - 1 && grid[bombs[i][0] + 1][bombs[i][1]] != 9) {
+            grid[bombs[i][0] + 1][bombs[i][1]] += 1;
         }
-        if (bombs[i][0] < grid.length-1  && bombs[i][1] < grid.length-1 && grid[bombs[i][0]+1][bombs[i][1]+1] != "X"){
-            grid[bombs[i][0]+1][bombs[i][1]+1] += 1;
-        }  
-        if (bombs[i][1] < grid.length-1 && grid[bombs[i][0]][bombs[i][1]+1] != "X"){
-            grid[bombs[i][0]][bombs[i][1]+1] += 1;
-        }  
-        if (bombs[i][0] > 0 && bombs[i][1] < grid.length-1 && grid[bombs[i][0]-1][bombs[i][1]+1] != "X"){
-            grid[bombs[i][0]-1][bombs[i][1]+1] += 1;
-        }      
-        if (bombs[i][0] > 0 && grid[bombs[i][0]-1][bombs[i][1]] != "X"){
-            grid[bombs[i][0]-1][bombs[i][1]] += 1;
-        }  
-        if (bombs[i][0] > 0 && bombs[i][1] > 0 && grid[bombs[i][0]-1][bombs[i][1]-1] != "X"){
-            grid[bombs[i][0]-1][bombs[i][1]-1] += 1;
-        }        
+        if (bombs[i][0] < grid.length - 1 && bombs[i][1] < grid.length - 1 && grid[bombs[i][0] + 1][bombs[i][1] + 1] != 9) {
+            grid[bombs[i][0] + 1][bombs[i][1] + 1] += 1;
+        }
+        if (bombs[i][1] < grid.length - 1 && grid[bombs[i][0]][bombs[i][1] + 1] != 9) {
+            grid[bombs[i][0]][bombs[i][1] + 1] += 1;
+        }
+        if (bombs[i][0] > 0 && bombs[i][1] < grid.length - 1 && grid[bombs[i][0] - 1][bombs[i][1] + 1] != 9) {
+            grid[bombs[i][0] - 1][bombs[i][1] + 1] += 1;
+        }
+        if (bombs[i][0] > 0 && grid[bombs[i][0] - 1][bombs[i][1]] != 9) {
+            grid[bombs[i][0] - 1][bombs[i][1]] += 1;
+        }
+        if (bombs[i][0] > 0 && bombs[i][1] > 0 && grid[bombs[i][0] - 1][bombs[i][1] - 1] != 9) {
+            grid[bombs[i][0] - 1][bombs[i][1] - 1] += 1;
+        }
+    }
+}
+
+/**S'occupe de l'affichage html du démineur
+ * 
+ * @param {*} myGrid est la grille qu'on va display
+ */
+function displayGrid(myGrid) {
+    for (let i = 0; i < myGrid.length; i++) {
+        document.getElementById("mainDivID").appendChild(document.createElement("div")).setAttribute("id", "div" + i);
+        for (let j = myGrid.length-1; j >= 0 ; j--) {
+            document.getElementById("div"+i).appendChild(document.createElement("div")).setAttribute("id", "div"+i+j);
+            document.getElementById("div"+i+j).setAttribute("class", "flexbox-item");
+            document.getElementById("div"+i+j).innerHTML = myGrid[myGrid.length-1-j][i];
+        }
     }
 }
 
